7c530fb22dcc70d36ffb257e521327bdea5c762c
J4viscript <tezjavi@gmail.com>
2023-02-23 14:17:49 -0700

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Javier Oskar Murillo Cota on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
